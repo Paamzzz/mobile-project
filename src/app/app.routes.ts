93d89dec8df5f2b3bbf5371645d0fea7f3e3837a
D3vJoy <168494262+J01C3@users.noreply.github.com>
2026-04-07 16:45:43 -0300

    Adicionando páginas e navegação entre telas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,12 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'cadastro',
+    loadComponent: () => import('./pages/cadastro/cadastro.page').then( m => m.CadastroPage)
+  },
 ];
