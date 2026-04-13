6099e5e268a93bd3c4403aab556d7ee7a7d641ef
João Macedo <joaoprbmacedo@gmail.com>
2026-04-13 13:33:31 -0300

    build: configuração da infraestrutura do Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA7jxdYk-i9NHmTkb4Cms3cxSntIskblUM",
+    authDomain: "projetoflashcards3.firebaseapp.com",
+    projectId: "projetoflashcards3",
+    storageBucket: "projetoflashcards3.firebasestorage.app",
+    messagingSenderId: "249600256129",
+    appId: "1:249600256129:web:8870fa8e798ecc2df44580",
+    measurementId: "G-H7Z4VJVHTE"
+  }
 };
 
 /*
