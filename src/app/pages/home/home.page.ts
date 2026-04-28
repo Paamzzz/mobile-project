b468955ce32e697f0f461e4c2ffc4b5026cb3d7c
Paamzzz <pamamanciogoulart@gmail.com>
2026-04-28 15:45:25 -0300

    feat: implementa rota de logout na Homepage

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
-import { RouterLink } from '@angular/router';
+import { RouterLink, Router } from '@angular/router'; // Router: serviço typescript        RouterLink: HTML redireciona
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,15 @@ import { RouterLink } from '@angular/router';
   styleUrls: ['home.page.scss'],
   imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent,  RouterLink],
 })
+
 export class HomePage {
-  constructor() {}
+  //! Estou tendo que colocar esta lógica aqui pois o componente de logout nao foi feito
+  //! ao ser concluído irei mover esta lógica para lá
+  private authService = inject(AuthService); // estamos dizendo que estes componentes tem acesso ao AuthService
+  private router = inject(Router);
+
+  async logout() { // enquanto o serviço logout executa...
+     await this.authService.sair(); // espera ESSE serviço executar X funçao (sair())
+     this.router.navigate(['/splash']);
+  }
 }
