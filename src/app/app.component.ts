7bcbf93e49f7c808e6d31b3a3ca6813864a294c0
Paamzzz <pamamanciogoulart@gmail.com>
2026-05-13 18:04:11 -0300

    feat: adicionar video gradient-mesh na pasta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-
 import { addIcons } from 'ionicons';
 import { logoGithub } from 'ionicons/icons';
 
